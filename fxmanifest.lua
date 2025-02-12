b6f1c7b199d18eb9860694a3dc6fb8f59a51d853
Rex-Development <rex.development.19@gmail.com>
2025-02-12 14:56:25 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Rex-Development'
-description 'Anti-Bunny-Hop Script By Rex
+description 'Anti-Bunny-Hop Script By Rex'
 version '1.0'
 
 client_script 'antibunnyhop.lua'
